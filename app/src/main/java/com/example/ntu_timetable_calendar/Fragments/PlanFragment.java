f13133415a86da93385ba8175615cddb2a69c311
Nicholas <nicholasganshyan@gmail.com>
2019-07-06 01:34:27 +0800

    add weekview/timetable widget

--- app/src/main/java/com/example/ntu_timetable_calendar/Fragments/PlanFragment.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Fragments/PlanFragment.java
@@ -1,5 +1,6 @@
 package com.example.ntu_timetable_calendar.Fragments;
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,18 +15,30 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.alamkanak.weekview.DateTimeInterpreter;
+import com.alamkanak.weekview.EventClickListener;
+import com.alamkanak.weekview.MonthChangeListener;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewDisplayable;
 import com.example.ntu_timetable_calendar.CourseModels.Course;
+import com.example.ntu_timetable_calendar.EventModel.Event;
 import com.example.ntu_timetable_calendar.R;
 import com.example.ntu_timetable_calendar.ViewModels.SearchViewModel;
 import com.google.android.material.button.MaterialButton;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class PlanFragment extends Fragment implements View.OnClickListener {
 
@@ -35,13 +48,16 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
     private List<String> courseSelectionsList = new ArrayList<>();
     private List<String> finalSelList = new ArrayList<>();
     private List<String> allCourseCodesList = new ArrayList<>();
-    // private List<Course> queriedCourseList = new ArrayList<>();
 
-    private static final String TAG = "PlanFragmentText";
+    // We store the events we want to display in the weekview widget here
+    List<WeekViewDisplayable> eventList = new ArrayList<>();
+
+    private static final String TAG = "PlanFragmentTAG";
 
     // Views
-    private MaterialButton planButton, clearButton;
+    private MaterialButton planButton, clearButton, chooseIndexesButton;
     private TextView errorTV;
+    private WeekView mWeekView;
 
     @Nullable
     @Override
@@ -54,6 +70,7 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
         initViewModel();
+        initWeekViewWidget();
     }
 
     private void initViews(View view) {
@@ -61,8 +78,12 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
         planButton.setOnClickListener(this);
         clearButton = view.findViewById(R.id.plan_fragment_clear_button);
         clearButton.setOnClickListener(this);
+        chooseIndexesButton = view.findViewById(R.id.plan_fragment_choose_indexes);
+        chooseIndexesButton.setOnClickListener(this);
+        mWeekView = view.findViewById(R.id.plan_fragment_weekView);
         errorTV = view.findViewById(R.id.plan_fragment_error_textview);
         multiAutoCompleteTextView = view.findViewById(R.id.plan_fragment_autocompletetextview);
+
     }
 
     private void initViewModel() {
@@ -88,15 +109,82 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
 
         /*
             Observe changes to the list of courses chosen by the user -- triggered when the user presses the plan button
+            and the viewmodel receives the list of courses according to the query
          */
         searchViewModel.getTimetablePlanningCourseList().observe(this, new Observer<List<Course>>() {
             @Override
             public void onChanged(List<Course> courseList) {
-                for (Course course : courseList) {
-                    Log.d(TAG, "onChanged: Course code : " + course.getCourseCode());
+                displayTimetable(courseList);
+            }
+        });
+    }
+
+    /**
+     * Functionality of the weekview widget is defined here
+     */
+    private void initWeekViewWidget() {
+
+        /*
+          IMPORTANT! - We pass the list of events that we want to display in the widget here.
+         */
+        mWeekView.setMonthChangeListener(new MonthChangeListener() {
+            @NotNull
+            @Override
+            public List<WeekViewDisplayable> onMonthChange(@NotNull Calendar calendar, @NotNull Calendar calendar1) {
+
+                // This sets up the WeekView widget to display starting from Monday and 0700 -- DO NOT MOVE THESE LINES OF CODE
+                Calendar cal = Calendar.getInstance();
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                mWeekView.goToDate(cal);
+                mWeekView.goToHour(7);
+
+
+
+                /*
+                This check is important -- without it, the same event will be duplicated three times as the library will
+                preload three months of events!
+                 */
+                Calendar today = Calendar.getInstance();
+                if (today.get(Calendar.MONTH) == calendar.get(Calendar.MONTH)) {
+                    return eventList;
+                } else {
+                    List<WeekViewDisplayable> emptyList = new ArrayList<>();
+                    return emptyList;
+                }
+
+            }
+        });
+
+        mWeekView.setOnEventClickListener(new EventClickListener() {
+            @Override
+            public void onEventClick(Object o, @NotNull RectF rectF) {
+
+            }
+        });
+
+        /*
+          This method affects how the time (vertical axis) and date/day (horizontal axis) are displayed. I override the
+          original implementation in order to remove the date as I only want to show day.
+         */
+        mWeekView.setDateTimeInterpreter(new DateTimeInterpreter() {
+            @NotNull
+            @Override
+            public String interpretDate(@NotNull Calendar calendar) {
+                SimpleDateFormat weekdayNameFormat = new SimpleDateFormat("EEE", Locale.getDefault());
+                return weekdayNameFormat.format(calendar.getTime());
+            }
+
+            @NotNull
+            @Override
+            public String interpretTime(int i) {
+                if (i < 10) {
+                    return "0" + i + ":00";
+                } else {
+                    return i + ":00";
                 }
             }
         });
+
     }
 
     /**
@@ -146,7 +234,8 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
      * Check if all of the user input courses are correct/exists by checking if all entered course codes can be
      * found in the allCourseCodesList.
      * <p>
-     * This function is called by "planTimetable(List<String> finalSelList)"
+     * This function is called when the plan button is pressed.
+     * <p>
      * Returns true if user input is valid, false if not
      */
     private boolean validationCheck() {
@@ -158,7 +247,11 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
             finalSelList.add(courseSelectionsList.get(i).trim().toUpperCase());
         }
 
-        if (allCourseCodesList.containsAll(finalSelList)) {
+        if (finalSelList.size() == 0) {
+            errorTV.setText(getString(R.string.error_message));
+            errorTV.setVisibility(View.VISIBLE);
+            return false;
+        } else if (allCourseCodesList.containsAll(finalSelList)) {
             errorTV.setVisibility(View.GONE);
             return true;
         } else {
@@ -169,14 +262,26 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
     }
 
     /**
-     * Called when plan fragment button is pressed -- send the list of all course codes chosen by the user to the viewmodel
+     * Takes in the list of queried courses and displays them in the timetable widget by 'converting' them
+     * into a list of event objects stored in the class member variable eventList.
+     * <p>
+     * Called in the searchViewModel.getTimetablePlanningCourseList() observable above.
+     *
+     * @param courseList
      */
-    private void planTimetable() {
-        if (validationCheck()) {
-            searchViewModel.queryPlanningTimetableCourses(finalSelList);
-        }
-    }
+    private void displayTimetable(List<Course> courseList) {
+        eventList.clear();
 
+       /* Calendar startTime = Calendar.getInstance();
+        Calendar endTime = (Calendar) startTime.clone();
+        startTime.add(Calendar.MINUTE, -120);
+
+        eventList.add(new Event(1, "Testing Event", startTime, endTime, "Testing Location",
+                ContextCompat.getColor(getContext(), R.color.event_color_03), false, false));*/
+
+        // Important -- Notifies the WeekView widget to refresh its data and display the new events/timetable
+        mWeekView.notifyDataSetChanged();
+    }
 
     @Override
     public void onClick(View view) {
@@ -186,7 +291,11 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
                 courseSelectionsList.clear();
                 break;
             case R.id.plan_fragment_plan_button:
-                planTimetable();
+                if (validationCheck()) {
+                    searchViewModel.queryPlanningTimetableCourses(finalSelList);
+                }
+                break;
+            case R.id.plan_fragment_choose_indexes:
                 break;
         }
     }

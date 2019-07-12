3ab498d8d69746c41428202d8a473e58bcd5b45e
Nicholas Gan <nicholasganshyan@gmail.com>
2019-07-13 00:29:35 +0800

    add entities

---
+++ app/src/main/java/com/example/ntu_timetable_calendar/Entity/ExamEntity.java
@@ -0,0 +1,74 @@
+package com.example.ntu_timetable_calendar.Entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "exam_table", foreignKeys = @ForeignKey(entity = TimetableEntity.class, parentColumns = "id",
+childColumns = "timeTableId"))
+public class ExamEntity {
+
+    @ColumnInfo(index = true)
+    private int timeTableId; // Id of the timetable this course belongs to is stored here
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String date;
+    private String day;
+    private String time;
+    private String code;
+    private String name;
+    private float duration;
+
+    public ExamEntity(int timeTableId, String date, String day, String time, String code, String name, float duration) {
+        this.timeTableId = timeTableId;
+        this.date = date;
+        this.day = day;
+        this.time = time;
+        this.code = code;
+        this.name = name;
+        this.duration = duration;
+    }
+
+    // Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getters
+
+    public int getTimeTableId() {
+        return timeTableId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+}
---
+++ app/src/main/java/com/example/ntu_timetable_calendar/Entity/TimetableEntity.java
@@ -0,0 +1,48 @@
+package com.example.ntu_timetable_calendar.Entity;
+
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "timetable_table", indices = {@Index(value = {"id"}, unique = true)})
+public class TimetableEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String name;
+    private String description;
+    private boolean isMainTimetable;
+
+    public TimetableEntity(String name, String description, boolean isMainTimetable) {
+        this.name = name;
+        this.description = description;
+        this.isMainTimetable = isMainTimetable;
+    }
+
+    // Setter
+
+    public void setId(int timetableId) {
+        this.id = timetableId;
+    }
+
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isMainTimetable() {
+        return isMainTimetable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}

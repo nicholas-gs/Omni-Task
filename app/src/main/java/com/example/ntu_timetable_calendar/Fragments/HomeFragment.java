e22126f37be96fa74f1ead28eeb705e3de9da74e
Nicholas <nicholasganshyan@gmail.com>
2019-07-18 11:54:50 +0800

    Minor modification to home fragment

--- app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
@@ -2,20 +2,38 @@ package com.example.ntu_timetable_calendar.Fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.ntu_timetable_calendar.R;
+import com.google.android.material.appbar.AppBarLayout;
 
 public class HomeFragment extends Fragment {
+
+    private AppBarLayout mAppbarLayout;
+    private Toolbar mToolbar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initViews(view);
+    }
+
+    private void initViews(View view){
+        mToolbar = view.findViewById(R.id.home_fragment_toolbar);
     }
 }

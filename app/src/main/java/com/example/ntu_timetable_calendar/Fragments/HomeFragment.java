a8555bd71d96b6737698bf658086ad4566dc9d95
Nicholas Gan <nicholasganshyan@gmail.com>
2019-07-23 18:14:09 +0800

    Add WeekView widget to HomeFragment

--- app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.ntu_timetable_calendar.Fragments;
 
 import android.content.Intent;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -12,14 +13,30 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.alamkanak.weekview.EventClickListener;
+import com.alamkanak.weekview.MonthChangeListener;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewDisplayable;
+import com.example.ntu_timetable_calendar.EventModel.Event;
 import com.example.ntu_timetable_calendar.R;
 import com.example.ntu_timetable_calendar.SecondActivity;
 import com.google.android.material.appbar.AppBarLayout;
 
-public class HomeFragment extends Fragment {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements EventClickListener<Event>,
+        MonthChangeListener<Event> {
 
     private AppBarLayout mAppbarLayout;
     private Toolbar mToolbar;
+    private WeekView<Event> mWeekView;
+
+    // We store the events we want to display in the weekview widget here
+    private List<WeekViewDisplayable<Event>> eventList = new ArrayList<>();
 
     @Nullable
     @Override
@@ -34,13 +51,14 @@ public class HomeFragment extends Fragment {
         initViews(view);
     }
 
-    private void initViews(View view){
-        mToolbar = view.findViewById(R.id.home_fragment_toolbar);
+    private void initViews(View view) {
+        mAppbarLayout = view.findViewById(R.id.home_fragment_appbarlayout);
 
+        mToolbar = view.findViewById(R.id.home_fragment_toolbar);
         mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.saved_timetables_menu_item:
                         startSecondActivity(getString(R.string.SAVED_TIMETABLES_INTENT));
                         break;
@@ -51,11 +69,46 @@ public class HomeFragment extends Fragment {
                 return true;
             }
         });
+
+        mWeekView = view.findViewById(R.id.home_fragment_weekview);
+        mWeekView.setMonthChangeListener(this);
+        mWeekView.setOnEventClickListener(this);
+        mWeekView.goToHour(8);
     }
 
-    private void startSecondActivity(String intentString){
+    private void startSecondActivity(String intentString) {
         Intent intent = new Intent(getActivity(), SecondActivity.class);
         intent.putExtra(getString(R.string.ACTIVITY_INTENT), intentString);
         startActivity(intent);
     }
+
+    /**
+     * WeekView widget event click listener
+     *
+     * @param event Event clicked
+     * @param rectF The drawable clicked
+     */
+    @Override
+    public void onEventClick(Event event, @NotNull RectF rectF) {
+
+    }
+
+    /**
+     * IMPORTANT! - We pass the list of events that we want to display in the WeekView widget here.
+     */
+    @NotNull
+    @Override
+    public List<WeekViewDisplayable<Event>> onMonthChange(@NotNull Calendar calendar, @NotNull Calendar calendar1) {
+           /*
+          This check is important -- without it, the same event will be duplicated three times as the library will
+          preload three months of events!
+         */
+        Calendar today = Calendar.getInstance();
+        if (today.get(Calendar.MONTH) == calendar.get(Calendar.MONTH)) {
+            return eventList;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
 }

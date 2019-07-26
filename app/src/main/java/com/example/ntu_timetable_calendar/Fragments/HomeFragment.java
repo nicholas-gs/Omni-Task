583903acaab1e102d4389b0743863fc5d00a9d5f
Nicholas Gan <nicholasganshyan@gmail.com>
2019-07-26 16:03:52 +0800

    Allow user to change number of visible days in WeekView widget/other minor UI changes

--- app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.ntu_timetable_calendar.Fragments;
 import android.content.Intent;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +20,7 @@ import com.alamkanak.weekview.EventClickListener;
 import com.alamkanak.weekview.MonthChangeListener;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewDisplayable;
+import com.example.ntu_timetable_calendar.Dialogs.TimetableEventDetailDialog;
 import com.example.ntu_timetable_calendar.Entity.CourseEventEntity;
 import com.example.ntu_timetable_calendar.Entity.TimetableEntity;
 import com.example.ntu_timetable_calendar.EventModel.Event;
@@ -32,6 +34,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment implements EventClickListener<Event>,
         MonthChangeListener<Event> {
@@ -40,7 +43,7 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
     private Toolbar mToolbar;
     private WeekView<Event> mWeekView;
 
-    // We store the events we want to display in the weekview widget here
+    // We store the events we want to display in the WeekView widget here
     private List<WeekViewDisplayable<Event>> eventList = new ArrayList<>();
 
     private SQLViewModel sqlViewModel;
@@ -55,8 +58,8 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initViews(view);
         sqlViewModel = ViewModelProviders.of(this).get(SQLViewModel.class);
+        initViews(view);
         getMainTimetable();
     }
 
@@ -68,6 +71,18 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
+                    case R.id.go_to_today_menu_item:
+                        goToNow();
+                        break;
+                    case R.id.day_view_menu_item:
+                        setNumberOfVisibleDays(item);
+                        break;
+                    case R.id.three_day_view_menu_item:
+                        setNumberOfVisibleDays(item);
+                        break;
+                    case R.id.five_day_view_menu_item:
+                        setNumberOfVisibleDays(item);
+                        break;
                     case R.id.saved_timetables_menu_item:
                         startSecondActivity(getString(R.string.SAVED_TIMETABLES_INTENT));
                         break;
@@ -82,14 +97,39 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
         mWeekView = view.findViewById(R.id.home_fragment_weekview);
         mWeekView.setMonthChangeListener(this);
         mWeekView.setOnEventClickListener(this);
-        mWeekView.goToHour(8);
     }
 
+    private void setNumberOfVisibleDays(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.day_view_menu_item:
+                mWeekView.setNumberOfVisibleDays(1);
+                break;
+            case R.id.three_day_view_menu_item:
+                mWeekView.setNumberOfVisibleDays(3);
+                break;
+            case R.id.five_day_view_menu_item:
+                mWeekView.setNumberOfVisibleDays(5);
+                break;
+        }
+        item.setChecked(true);
+    }
 
+    /**
+     * Go to current day and time for the WeekView widget
+     */
+    private void goToNow() {
+        mWeekView.goToToday();
+        mWeekView.goToCurrentTime();
+    }
+
+    /**
+     * Since we are only going to display the main timetable in the WeekView widget, the will get only it from Room
+     */
     private void getMainTimetable() {
         sqlViewModel.getMainTimetable().observe(this, new Observer<TimetableEntity>() {
             @Override
             public void onChanged(TimetableEntity timetableEntity) {
+                // TimetableEntity may be null if the user does not have a saved timetable set as main
                 if (timetableEntity != null) {
                     getEvents(timetableEntity);
                 }
@@ -97,8 +137,12 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
         });
     }
 
+    /**
+     * Get all the CourseEventEntities belonging to the main timetable
+     *
+     * @param timetableEntity Main timetable from Room
+     */
     private void getEvents(TimetableEntity timetableEntity) {
-
         sqlViewModel.getTimetableCourseEvents(timetableEntity.getId()).observe(this, new Observer<List<CourseEventEntity>>() {
             @Override
             public void onChanged(List<CourseEventEntity> courseEventEntityList) {
@@ -107,8 +151,19 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
         });
     }
 
+    /**
+     * Takes in a list of CourseEventEntity from Room and converts them into Event for display in the WeekView widget
+     *
+     * @param courseEventEntityList List of CourseEventEntity from Room
+     */
     private void displayEvents(List<CourseEventEntity> courseEventEntityList) {
         this.eventList.clear();
+
+        // Set the size factor of the WeekView widget (simulate pinching the widget). The user can still adjust the size/
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        Objects.requireNonNull(getActivity()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        mWeekView.setHourHeight(height / 12f);
 
         Calendar calendar = Calendar.getInstance();
 
@@ -141,7 +196,8 @@ public class HomeFragment extends Fragment implements EventClickListener<Event>,
      */
     @Override
     public void onEventClick(Event event, @NotNull RectF rectF) {
-
+        TimetableEventDetailDialog dialog = new TimetableEventDetailDialog(event);
+        dialog.show(getChildFragmentManager(), "timetable_event_detail_dialog");
     }
 
     /**

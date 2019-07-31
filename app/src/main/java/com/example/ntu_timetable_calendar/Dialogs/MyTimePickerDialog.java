947cc3f32fd220c72d1bb648636b03da92c7aa33
Nicholas Gan <nicholasganshyan@gmail.com>
2019-07-31 22:04:16 +0800

    Fix TimePickerDialog orientation change crash

--- app/src/main/java/com/example/ntu_timetable_calendar/Dialogs/MyTimePickerDialog.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Dialogs/MyTimePickerDialog.java
@@ -4,11 +4,12 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.example.ntu_timetable_calendar.R;
 
 import java.util.Calendar;
 
@@ -17,16 +18,32 @@ public class MyTimePickerDialog extends DialogFragment {
     // Pass in a calendar variable to set the current/chosen time
     private Calendar calendar;
 
-    public MyTimePickerDialog(Calendar calendar){
+    public MyTimePickerDialog(Calendar calendar) {
         this.calendar = calendar;
+    }
+
+    public MyTimePickerDialog() {
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new TimePickerDialog(requireContext(), (TimePickerDialog.OnTimeSetListener) getParentFragment(),
-                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), DateFormat.is24HourFormat(requireContext()));
+        if (savedInstanceState == null) {
+            return new TimePickerDialog(requireContext(), (TimePickerDialog.OnTimeSetListener) getParentFragment(),
+                    calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), DateFormat.is24HourFormat(requireContext()));
+        } else {
+            long l = savedInstanceState.getLong(getString(R.string.time_picker_dialog_saved_calendar_key));
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(l);
+            this.calendar = c;
+            return new TimePickerDialog(requireContext(), (TimePickerDialog.OnTimeSetListener) getParentFragment(),
+                    calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), DateFormat.is24HourFormat(requireContext()));
+        }
     }
 
-
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong(getString(R.string.time_picker_dialog_saved_calendar_key), this.calendar.getTimeInMillis());
+        super.onSaveInstanceState(outState);
+    }
 }

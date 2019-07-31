d8a0c7a81e8aa1866734133140fcca6bf6a00abc
Nicholas Gan <nicholasganshyan@gmail.com>
2019-07-31 21:53:50 +0800

    Fix DatePickerDialog orientation change crash

--- app/src/main/java/com/example/ntu_timetable_calendar/Dialogs/MyDatePickerDialog.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Dialogs/MyDatePickerDialog.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.ntu_timetable_calendar.R;
+
 import java.util.Calendar;
 
 public class MyDatePickerDialog extends DialogFragment {
@@ -17,10 +19,28 @@ public class MyDatePickerDialog extends DialogFragment {
         this.calendar = calendar;
     }
 
+    public MyDatePickerDialog() {
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new android.app.DatePickerDialog(requireContext(), (android.app.DatePickerDialog.OnDateSetListener) getParentFragment()
-                , calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        if (savedInstanceState == null) {
+            return new android.app.DatePickerDialog(requireContext(), (android.app.DatePickerDialog.OnDateSetListener) getParentFragment()
+                    , calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        } else {
+            long l = savedInstanceState.getLong(getString(R.string.date_picker_dialog_saved_calendar_key));
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(l);
+            this.calendar = c;
+            return new android.app.DatePickerDialog(requireContext(), (android.app.DatePickerDialog.OnDateSetListener) getParentFragment()
+                    , calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong(getString(R.string.date_picker_dialog_saved_calendar_key), this.calendar.getTimeInMillis());
+        super.onSaveInstanceState(outState);
     }
 }

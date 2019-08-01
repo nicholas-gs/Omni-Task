bca06877bf783a3b41a4c1eb51a3de4a1836be1e
Nicholas Gan <nicholasganshyan@gmail.com>
2019-08-01 13:57:10 +0800

    Implement RecyclerView for TaskFragmentFrag1

--- app/src/main/java/com/example/ntu_timetable_calendar/Fragments/TaskFragmentFrag1.java
+++ app/src/main/java/com/example/ntu_timetable_calendar/Fragments/TaskFragmentFrag1.java
@@ -8,14 +8,102 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ntu_timetable_calendar.Entity.TaskEntity;
 import com.example.ntu_timetable_calendar.R;
+import com.example.ntu_timetable_calendar.RVAdapters.TaskFragmentFrag1RVAdapter;
+import com.example.ntu_timetable_calendar.ViewModels.SQLViewModel;
 
-public class TaskFragmentFrag1 extends Fragment {
+import java.util.Calendar;
+import java.util.List;
+
+public class TaskFragmentFrag1 extends Fragment implements TaskFragmentFrag1RVAdapter.onItemClickedListener {
+
+    // Views
+    private RecyclerView mRecyclerView;
+
+    private TaskFragmentFrag1RVAdapter mAdapter;
+
+    // ViewModels
+    private SQLViewModel sqlViewModel;
+
+    // Variables
+    private Calendar nowCalendar;
+    private Calendar deadlineCalendar;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.task_fragment_frag1_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initViews(view);
+        setUpRecyclerView();
+        initVariables();
+        initViewModels();
+    }
+
+    private void initViews(View view) {
+        mRecyclerView = view.findViewById(R.id.tasks_frag1_recyclerview);
+    }
+
+    private void setUpRecyclerView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
+        this.mAdapter = new TaskFragmentFrag1RVAdapter(requireContext());
+        this.mAdapter.setOnItemClickedListener(this);
+        mRecyclerView.setAdapter(this.mAdapter);
+    }
+
+    private void initVariables() {
+        this.nowCalendar = Calendar.getInstance();
+        this.deadlineCalendar = (Calendar) nowCalendar.clone();
+        this.deadlineCalendar.add(Calendar.DAY_OF_YEAR, 7);
+    }
+
+    private void initViewModels() {
+        sqlViewModel = ViewModelProviders.of(this).get(SQLViewModel.class);
+
+        sqlViewModel.getTasksWithinTime(this.nowCalendar.getTimeInMillis(), this.deadlineCalendar.getTimeInMillis())
+                .observe(this, new Observer<List<TaskEntity>>() {
+                    @Override
+                    public void onChanged(List<TaskEntity> taskEntities) {
+                        passData(taskEntities);
+                    }
+                });
+    }
+
+    /**
+     * Pass data to the RecyclerView's adapter for display
+     *
+     * @param taskEntities List of TaskEntity from SQLViewModel
+     */
+    private void passData(List<TaskEntity> taskEntities) {
+        if (taskEntities != null) {
+            this.mAdapter.submitList(taskEntities);
+        }
+    }
+
+    /**
+     * Callback method for RecyclerView item clicked
+     *
+     * @param taskEntity TaskEntity object clicked
+     * @param position   Index of RecyclerView ViewHolder clicked
+     */
+    @Override
+    public void onItemClicked(TaskEntity taskEntity, int position) {
+
     }
 }
